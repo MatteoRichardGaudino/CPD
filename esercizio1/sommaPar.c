46aa634c68fc36df73ffebf6d67166aae8cf7cc1
Claudio <cladotti31@gmail.com>
2023-10-15 14:16:54 +0200

    - Modificato sommaPar.c per rispettare i nuovi criteri di input - Modificato progetto1.pbs per richiamare correttamente sommaPar.c - Eliminata un sacco di roba pt. 1

--- esercizio1/sommaPar.c
+++ esercizio1/sommaPar.c
@@ -8,49 +8,31 @@
 #include <stdlib.h> // malloc, atoi
 #include <time.h> // Per la generazione di numeri random
 #include <math.h> // log2 
-#include <unistd.h> // getopt, optarg
 
 /**
  * Utilizzo:
  *  mpiexec -machinefile <hostFile> -np <p> sommaPar <...>
  * 
  * Sistassi
- *  sommaPar [-s strategy] [-o output_rank] -f filename
- *  sommaPar [-s strategy] [-o output_rank] -r numToSum
- *  sommaPar [-s strategy] [-o output_rank] -n numToSum num_1 ... num_n
+ *  sommaPar strategy numToSum                          (numToSum > 20)
+ *  sommaPar strategy numToSum num_1 ... num_n          (numToSum <= 20)
  * 
  * Semantica
- * 
- *  -s stabilisce la strategia da utilizzare 1, 2 o 3. 
+ *  strategy stabilisce la strategia da utilizzare 1, 2 o 3.
  *     Nel caso il numero di processori non sia una potenza di 2 verrà utilizzata la strategia 1.
- *     Il valore di default è 1. OPZIONALE
- * 
- *  -o Stabilisce quale processo deve scrivere i risultati su stdout.
- *     Se il valore è -1 tutti i processi scriveranno i propri risultati.
- *     Il valore di default è 0. OPZIONALE
- * 
- *  -f Il programma dovrà prendere i numeri da sommare in un file di input
- * 
- *  -r Il programma dovrà generare <numToSum> numeri random e sommarli
- * 
- *  -n Il programma prenderà in input <numToSum> numeri da sommare da riga di comando.
- *     I umeri devono essere separati da uno spazio vuoto
- *   
- *  Attenzione! -f -r -n sono mutuamente esclusivi. L'ordine dei flag non è rilevante
- * 
+ *     Il valore di default è 1.
+ *
+ *
+ *  Il programma prenderà in input <numToSum> numeri da sommare da riga di comando, se numToSum <=20.
+ *  In alternativa, i numeri saranno generati casualmente.
+ *     I numeri devono essere separati da uno spazio
+ *
  * **/
 
-// Stabilisce che Strategia di input è stata scelta
-enum INPUT_STRATEGY{
-    FILE_STRAT, // se usato -f
-    RANDOM_STRAT, // se usato -r
-    ARG_STRAT // se usato -n
-};
 
 int main(int argc, char** argv){
 
     MPI_Init(&argc, &argv);
-
 
     int worldSize, rank; // dimensione di MPI_COMM_WORLD e rank nel communicator
     MPI_Status status; // Per MPI_Recv
@@ -66,81 +48,46 @@ int main(int argc, char** argv){
     int* nums_v; // vettore da sommare
 
     int strategy = 1; // strategia per la comunicazione
-    int outputRank = 0; // Processo che stampa l'output
 
     if(rank == 0){
-        enum INPUT_STRATEGY in_strat; // strategia di input
-        FILE* testFile; // per -f
-        int opindex; // per -n
-
-        int op;
-        // Parsing dell'input
-        while ((op = getopt(argc, argv, "s:o:f:r:n:")) != -1){
-            switch (op){
-            case 's':
-                strategy = atoi(optarg);
-            break;
-            case 'o':
-                outputRank = atoi(optarg);
-            break;
-
-            case 'f':
-                testFile = fopen(optarg, "r"); // Apre il file di input in sola lettura
-                in_strat = FILE_STRAT;
-            break;
-
-            case 'r':
-                nums_c = atoi(optarg);
-                in_strat = RANDOM_STRAT;
-            break;
-
-            case 'n':
-                nums_c = atoi(optarg);
-                in_strat = ARG_STRAT;
-                opindex = optind;
-            break;
-            default:
-                perror("Wrong input, read the Documentation.\n");
-                MPI_Abort(MPI_COMM_WORLD, -1);
-                break;
-            }
+        int opindex = 0; // indice per scorrere le opzioni
+        if(argc < 3){
+            perror("Too few options. Read documentation.\n");
+            MPI_Abort(MPI_COMM_WORLD, -1);
         }
 
-        switch (in_strat){
-            case FILE_STRAT: // legge l'input da file
-                fscanf(testFile, "%d" ,&nums_c);
+        strategy = atoi(argv[1]);
+        nums_c = atoi(argv[2]);
 
-                nums_v = malloc(sizeof(int)*nums_c);
-                for (int i = 0; i < nums_c; i++){
-                    fscanf(testFile, "%d", &(nums_v[i]));
-                }
-                fclose(testFile);
-            break;
-            case RANDOM_STRAT:{ 
-                srand(time(NULL)); // Genera numeri casuali 
-                unsigned long long max = 1UL << 32; // di massimo 32 bit
+        if(strategy!=1 && strategy!=2 && strategy!=3){
+            perror("Warning, strategy is a number other than 1, 2 or 3. Strategy is set to 1\n");
+            strategy = 1;
+        }
 
-                nums_v = malloc(sizeof(int)*nums_c);
+        nums_v = malloc(sizeof(int)*nums_c);
 
-                for (int i = 0; i < nums_c; i++){
-                    nums_v[i] = rand()%max;
-                }
+        if(nums_c <= 20){       // Prendi numeri da linea di comando
+            if(argc - 3 != nums_c){
+                perror("Too many options. Read documentation.\n");
+                free(nums_v);
+                MPI_Abort(MPI_COMM_WORLD, -1);
             }
-            break;
-            case ARG_STRAT:{
-                    nums_v = malloc(sizeof(int)*nums_c);
 
-                    int i = 0;
-                    while (opindex < argc && *argv[opindex] != '-'){ // Legge dagli argomenti fichè non terminano
-                        nums_v[i++] = atoi(argv[opindex++]); // Un input sbagliato porta a segmentation fault
-                    }
-                }
-            break;
+            for(int i=0; i < nums_c; i++){
+                nums_v[i] = atoi(argv[i+3]);
+            }
+        } else {                  // Genera numeri casuali
+            srand(time(NULL)); // Genera numeri casuali
+            unsigned long long max = 1UL << 32; // di massimo 32 bit
+
+            for (int i = 0; i < nums_c; i++){
+                nums_v[i] = rand()%max;
+            }
         }
 
         if((worldSize & (worldSize - 1)) != 0 && strategy != 1) { // Verifica se il numero dei processori è una potenza di 2
             strategy = 1; // se non lo è si setta la strategia a 1
-            printf("Warning strategy is not a power of 2, strategy is set to 1\n");
+            perror("Warning strategy is not a power of 2, strategy is set to 1\n");
         }
     }
 
@@ -148,7 +95,7 @@ int main(int argc, char** argv){
 
     MPI_Bcast(&nums_c, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&strategy, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&outputRank, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
 
     int rest = nums_c % worldSize;
     int numToSum = nums_c/worldSize + ((rank < rest)? 1: 0); // numeri da sommare per ogni processo
@@ -156,7 +103,7 @@ int main(int argc, char** argv){
     if(rank == 0){
         int tmp = nums_c/worldSize; // Minimo numeri da sommare
         int cursor = numToSum;
-         
+
         for (int i = 1; i < worldSize; ++i){
             int inumToSum = tmp + ((i < rest)? 1: 0); // aggiunge 1 se i < rest
             MPI_Send(nums_v + cursor, inumToSum, MPI_INT, i, 0, MPI_COMM_WORLD);
@@ -168,12 +115,12 @@ int main(int argc, char** argv){
         MPI_Recv(nums_v, numToSum, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
     }
 
+// ----------------------- Creazione dei timer ----------------------
 
     double t0, t1; // Per misurare i tempi
     MPI_Barrier(MPI_COMM_WORLD);
     t0 = MPI_Wtime(); // è partita la misurazione
 
-    
 // ----------------------- Calcolo ----------------------
 
     long long sum = 0;
@@ -182,6 +129,7 @@ int main(int argc, char** argv){
     }
 
 /* ----------------------- Soluzione 1 ---------------------- */
+
     if(strategy == 1){
         if (rank != 0){ // se non è p0 invia
             MPI_Send(&sum, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
@@ -227,21 +175,18 @@ int main(int argc, char** argv){
     }
     
 /* ----------------------- Comunicazione dei risultati ---------------------- */
-    t1 = MPI_Wtime(); // fine della misurazione
+    t1 = MPI_Wtime();
     MPI_Barrier(MPI_COMM_WORLD);
 
     double localTime = t1-t0;
     double maxTime;
     
     MPI_Reduce(&localTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD); // calcola il tempo massimo impiegato
-    if(rank == 0) localTime = maxTime;
 
-    if (outputRank == -1){ // stampano tutti
-        printf("[%d] %lld %.10f\n", rank, sum, localTime);
-    } else if (rank == outputRank){ // stampa solo il processo selezionato
-        printf("[%d] %lld %.10f\n", rank, sum, localTime);
-    }
+    if(rank == 0)
+        printf("[%d] %lld %.10f\n", rank, sum, maxTime);
 
+    free(nums_v);
     MPI_Finalize();
     return 0;
 }
